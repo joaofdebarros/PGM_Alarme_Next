880781c9179ac55d03c994fbba6e0e5d2c2583fd
joaofdebarros <joaofdebarros@gmail.com>
2025-08-22 10:22:39 -0300

    feat: criação do arquivo header do main.c

---
+++ main.h
@@ -0,0 +1,177 @@
+/*
+ * main.h
+ *
+ *  Created on: 22 de ago. de 2025
+ *      Author: joao.victor
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "XMC1100.h"
+#include "cy_utils.h"
+#include "cybsp.h"
+#include "cycfg_peripherals.h"
+#include "cycfg_pins.h"
+#include "xmc1_gpio.h"
+#include "xmc_gpio.h"
+#include "xmc_uart.h"
+#include "xmc_usic.h"
+#include <stdint.h>
+#include <xmc_flash.h>
+
+#define FLASH_SECTOR_ADDR 0x10008F00U // Endereço de setor livre
+
+#define SIZE 0
+#define IDT 1
+#define UNIQUEID 2
+#define FUNCTION 3
+#define DATA 4
+#define CHECKSUM 5
+
+#define PGM_PACKET_HEADER_LEN 1
+#define PGM_PACKET_LENGTH_LEN 1
+#define PGM_PACKET_ID_LEN 1
+#define PGM_PACKET_ADDRESS_LEN 1
+#define PGM_PACKET_FUNCTION_LEN 1
+#define PGM_PACKET_TAIL_LEN 1
+#define PGM_PACKET_CHECKSUM_LEN 1
+
+#define TAMANHO_BUFFER 8
+#define TAMANHO_BUFFER_ACK 12
+
+uint8_t ACK = 0x06;
+uint8_t NAK = 0x06;
+uint8_t incremento = 0;
+uint8_t buffer_size = 8;
+uint8_t package_size = 0;
+// Buffer de envio e recepção de dados
+uint8_t Rx_buffer[TAMANHO_BUFFER_ACK];
+uint8_t Rx_buffer_index = 0;
+uint8_t Buffer_TX[TAMANHO_BUFFER_ACK] = {0};
+
+typedef enum {
+  PGM_ID = 0x01,
+  PGM_BROADCAST_ID = 0x02,
+  PGM_ID_RESPONSE = 0x03,
+} PGM_DEVICE_ID_t;
+
+typedef enum {
+  PGM_REGISTER = 0x00,
+  PGM_TOGGLE = 0x01,
+  PGM_STATUS = 0x02,
+  PGM_DELETE = 0x03,
+  PGM_RETRY_CRC = 0x04,
+} PGM_FUNCTION_t;
+
+typedef enum {
+  PGM_PACKET_OK,
+  PGM_PACKET_FAIL_HEADER,
+  PGM_PACKET_FAIL_TAIL,
+  PGM_PACKET_FAIL_LENGHT,
+  PGM_PACKET_FAIL_CHECKSUM,
+  PGM_PACKET_FAIL_UNKNOWN = 0xFF
+} pgm_packet_error_e;
+
+typedef struct {
+  uint8_t len;
+  uint8_t id;
+  uint8_t address;
+  uint8_t function;
+  uint8_t data[5];
+  uint8_t checksum;
+  uint8_t tail;
+} pgm_alarm_packet_t;
+
+typedef struct {
+  uint8_t len;
+  uint8_t id;
+  uint8_t function;
+  uint8_t data[5];
+  uint8_t checksum;
+  uint8_t tail;
+} pgm_gate_packet_t;
+
+uint32_t UniqueChipID[4] = {0, 0, 0, 0};
+uint8_t crc_address = 0;
+
+// Flags de envio e recepção de dados
+volatile bool alarm_packet_completed = false;
+volatile bool gate_packet_completed = false;
+volatile bool recebendo = false;
+volatile bool pacote_obsoleto = false;
+volatile bool cadastrado = false;
+bool new_alarm_packet = false;
+bool new_gate_packet = false;
+
+// Variáveis do pacote
+#define start_byte 0x7E
+uint8_t UID0 = 0;
+uint8_t UID1 = 0;
+uint8_t UID2 = 0;
+uint8_t UID3 = 0;
+uint8_t checksum = 0;
+#define stop_byte 0x81
+
+uint8_t checksum_validate = 0;
+volatile bool checksum_ok = false;
+
+// Máquina de estado_alarms
+uint8_t estado_alarm = 0;
+uint8_t estado_gate = 0;
+
+// Variáveis de tempo/delays
+uint8_t num_aleatorio = 200;
+uint16_t delay_aleatorio = 0;
+uint8_t Blinking_gap = 10;
+uint16_t Blink_wait = 1000;
+uint8_t piscadas = 0;
+uint8_t numero_modulo = 0;
+volatile uint32_t systick = 0;
+volatile uint32_t delay_tx = 0;
+volatile bool aguardando_envio = false;
+volatile bool piscando = false;
+
+// União bitflag
+typedef union {
+  uint8_t Byte;
+  struct {
+    bool rele_1 : 1;
+    bool rele_2 : 1;
+    bool rele_3 : 1;
+    bool rele_4 : 1;
+    bool rele_5 : 1;
+    bool bitflag5 : 1;
+    bool bitflag6 : 1;
+    bool bitflag7 : 1;
+  } Bits;
+} FLAG_Bits;
+
+typedef struct {
+  XMC_GPIO_PORT_t *port;
+  uint8_t pin;
+  uint8_t blink_count;
+  uint8_t blink_target;
+  bool piscando;
+} LED_t;
+
+LED_t leds[1] = {{LED_ST_PORT, LED_ST_PIN, 0, 0, false}};
+
+XMC_GPIO_PORT_t *const rele_ports[5] = {RL1_PORT, RL2_PORT, RL3_PORT, RL4_PORT,
+                                        RL5_PORT};
+
+const uint8_t rele_pins[5] = {RL1_PIN, RL2_PIN, RL3_PIN, RL4_PIN, RL5_PIN};
+
+typedef struct {
+  pgm_packet_error_e pgm_error;
+  pgm_alarm_packet_t alarm_packet;
+  pgm_gate_packet_t gate_packet;
+  FLAG_Bits status_rele, ligar_rele;
+} pgm_t;
+
+pgm_t pgm;
+
+
+
+
+#endif /* MAIN_H_ */
